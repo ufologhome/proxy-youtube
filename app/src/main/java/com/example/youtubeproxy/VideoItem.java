13fa44502a99e468cb5d9dbf85413c0f53a20151
ufologhome <38780109+ufologhome@users.noreply.github.com>
2025-12-30 20:22:12 +0300

    Create VideoItem.java

---
+++ app/src/main/java/com/example/youtubeproxy/VideoItem.java
@@ -0,0 +1,13 @@
+package com.example.youtubeproxy;
+
+public class VideoItem {
+    public String id;
+    public String title;
+    public String thumb;
+
+    public VideoItem(String id, String title, String thumb) {
+        this.id = id;
+        this.title = title;
+        this.thumb = thumb;
+    }
+}
