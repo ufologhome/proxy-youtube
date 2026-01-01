b0c3d62eb136fe040bc0579af1aecb78c671afd9
ufologhome <38780109+ufologhome@users.noreply.github.com>
2026-01-01 14:31:09 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/youtubeproxy/MainActivity.java
+++ app/src/main/java/com/example/youtubeproxy/MainActivity.java
@@ -4,26 +4,49 @@ import android.content.Intent;
 import android.net.VpnService;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle b) {
-        super.onCreate(b);
+    private static final int VPN_REQUEST_CODE = 0;
 
-        Intent vpn = VpnService.prepare(this);
-        if (vpn != null) {
-            startActivityForResult(vpn, 0);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Запускаем VPN
+        Intent intent = VpnService.prepare(this);
+        if (intent != null) {
+            startActivityForResult(intent, VPN_REQUEST_CODE);
         } else {
             startService(new Intent(this, MyVpnService.class));
-            startActivity(new Intent(this, PlayerActivity.class));
         }
+
+        // Список видео
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ArrayList<VideoItem> videos = new ArrayList<>();
+        videos.add(new VideoItem("BigBuckBunny", "Big Buck Bunny",
+                "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg"));
+
+        VideoAdapter adapter = new VideoAdapter(videos, video -> {
+            Intent i = new Intent(this, PlayerActivity.class);
+            startActivity(i);
+        });
+
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
-    protected void onActivityResult(int r, int c, Intent d) {
-        super.onActivityResult(r, c, d);
-        startService(new Intent(this, MyVpnService.class));
-        startActivity(new Intent(this, PlayerActivity.class));
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == VPN_REQUEST_CODE && resultCode == RESULT_OK) {
+            startService(new Intent(this, MyVpnService.class));
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
