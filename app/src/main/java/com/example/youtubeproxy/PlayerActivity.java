ca6edde8964ac14f1bc9a744b3b7608ac0ea4c0e
ufologhome <38780109+ufologhome@users.noreply.github.com>
2026-01-01 14:29:54 +0300

    Update PlayerActivity.java

--- app/src/main/java/com/example/youtubeproxy/PlayerActivity.java
+++ app/src/main/java/com/example/youtubeproxy/PlayerActivity.java
@@ -3,7 +3,6 @@ package com.example.youtubeproxy;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.ui.PlayerView;
@@ -13,21 +12,18 @@ public class PlayerActivity extends AppCompatActivity {
     private ExoPlayer player;
 
     @Override
-    protected void onCreate(Bundle b) {
-        super.onCreate(b);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
 
-        PlayerView view = new PlayerView(this);
-        setContentView(view);
+        PlayerView view = findViewById(R.id.playerView);
 
         player = new ExoPlayer.Builder(this).build();
         view.setPlayer(player);
 
-        // ⚠️ ПРОСТОЙ HTTP MP4 (НЕ HTTPS, НЕ YT)
-        Uri uri = Uri.parse(
-            "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-        );
-
-        player.setMediaItem(MediaItem.fromUri(uri));
+        // BigBuckBunny через VPN
+        String url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        player.setMediaItem(MediaItem.fromUri(Uri.parse(url)));
         player.prepare();
         player.play();
     }
