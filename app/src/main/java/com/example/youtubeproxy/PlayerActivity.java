813dc9a357549f47624a4450ac18c7933cfacc99
ufologhome <38780109+ufologhome@users.noreply.github.com>
2026-01-01 15:30:03 +0300

    Update PlayerActivity.java

--- app/src/main/java/com/example/youtubeproxy/PlayerActivity.java
+++ app/src/main/java/com/example/youtubeproxy/PlayerActivity.java
@@ -6,14 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
-import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSource;
-import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
 import com.google.android.exoplayer2.ui.PlayerView;
-
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-
-import okhttp3.OkHttpClient;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -26,26 +19,10 @@ public class PlayerActivity extends AppCompatActivity {
 
         PlayerView view = findViewById(R.id.playerView);
 
-        // Настройка Proxy к Go серверу
-        Proxy proxy = new Proxy(
-                Proxy.Type.HTTP,
-                new InetSocketAddress("192.168.0.150", 8881) // IP ПК с Go
-        );
-
-        OkHttpClient client = new OkHttpClient.Builder()
-                .proxy(proxy)
-                .build();
-
-        OkHttpDataSource.Factory factory =
-                new OkHttpDataSource.Factory(client);
-
-        player = new ExoPlayer.Builder(this)
-                .setMediaSourceFactory(new DefaultMediaSourceFactory(factory))
-                .build();
-
+        player = new ExoPlayer.Builder(this).build();
         view.setPlayer(player);
 
-        String url = getIntent().getStringExtra("video_url");
+        String url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
         player.setMediaItem(MediaItem.fromUri(Uri.parse(url)));
         player.prepare();
         player.play();
