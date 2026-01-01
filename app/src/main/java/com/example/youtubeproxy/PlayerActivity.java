c697fd8cfe9ae71788a77774d4a301de72ffbdad
ufologhome <38780109+ufologhome@users.noreply.github.com>
2026-01-01 12:00:19 +0300

    Update PlayerActivity.java

--- app/src/main/java/com/example/youtubeproxy/PlayerActivity.java
+++ app/src/main/java/com/example/youtubeproxy/PlayerActivity.java
@@ -7,41 +7,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.ui.PlayerView;
-import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSource;
-import com.google.android.exoplayer2.source.DefaultMediaSourceFactory;
-
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-
-import okhttp3.OkHttpClient;
 
 public class PlayerActivity extends AppCompatActivity {
 
     private ExoPlayer player;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
         setContentView(R.layout.activity_player);
 
         String videoId = getIntent().getStringExtra("video_id");
-        String url = "https://www.youtube.com/watch?v=xCLqc-nMc1o" + videoId;
 
-        Proxy proxy = new Proxy(
-                Proxy.Type.HTTP,
-                new InetSocketAddress("127.0.0.1", 8881) // <-- ТВОЙ GO PROXY
-        );
+        String url = "https://www.youtube.com/watch?v=" + videoId;
 
-        OkHttpClient client = new OkHttpClient.Builder()
-                .proxy(proxy)
-                .build();
-
-        OkHttpDataSource.Factory factory =
-                new OkHttpDataSource.Factory(client);
-
-        player = new ExoPlayer.Builder(this)
-                .setMediaSourceFactory(new DefaultMediaSourceFactory(factory))
-                .build();
+        player = new ExoPlayer.Builder(this).build();
 
         PlayerView view = findViewById(R.id.playerView);
         view.setPlayer(player);
