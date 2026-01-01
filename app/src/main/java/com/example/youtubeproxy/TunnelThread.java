a02dad590c973fd98b63c5156849295e2dc5c9cb
ufologhome <38780109+ufologhome@users.noreply.github.com>
2026-01-01 12:47:37 +0300

    Update TunnelThread.java

--- app/src/main/java/com/example/youtubeproxy/TunnelThread.java
+++ app/src/main/java/com/example/youtubeproxy/TunnelThread.java
@@ -8,21 +8,22 @@ public class TunnelThread extends Thread {
     private final InputStream in;
     private final OutputStream out;
 
-    public TunnelThread(InputStream in, OutputStream out) {
-        this.in = in;
-        this.out = out;
+    public TunnelThread(InputStream i, OutputStream o) {
+        in = i;
+        out = o;
     }
 
     @Override
     public void run() {
-        byte[] buffer = new byte[32767];
-        int len;
-
+        byte[] buf = new byte[32767];
         try {
-            while ((len = in.read(buffer)) > 0) {
-                out.write(buffer, 0, len);
+            int n;
+            while ((n = in.read(buf)) > 0) {
+                out.write(buf, 0, n);
                 out.flush();
             }
-        } catch (Exception ignored) {}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
