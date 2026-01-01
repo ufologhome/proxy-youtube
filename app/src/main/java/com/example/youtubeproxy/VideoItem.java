f4f9ba65902acc4e3a8beba812edd1d040c7bd6f
ufologhome <38780109+ufologhome@users.noreply.github.com>
2026-01-01 15:03:41 +0300

    Update VideoItem.java

--- app/src/main/java/com/example/youtubeproxy/VideoItem.java
+++ app/src/main/java/com/example/youtubeproxy/VideoItem.java
@@ -4,10 +4,12 @@ public class VideoItem {
     public String id;
     public String title;
     public String thumb;
+    public String url;
 
-    public VideoItem(String id, String title, String thumb) {
+    public VideoItem(String id, String title, String thumb, String url) {
         this.id = id;
         this.title = title;
         this.thumb = thumb;
+        this.url = url;
     }
 }
