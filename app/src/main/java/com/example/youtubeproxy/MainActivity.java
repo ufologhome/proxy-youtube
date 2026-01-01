a35371d0da457e0b263ffc4fc9f151e18f8eac42
ufologhome <38780109+ufologhome@users.noreply.github.com>
2026-01-01 12:46:31 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/youtubeproxy/MainActivity.java
+++ app/src/main/java/com/example/youtubeproxy/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.youtubeproxy;
 import android.content.Intent;
 import android.net.VpnService;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,13 +10,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle b) {
         super.onCreate(b);
-        setContentView(R.layout.activity_main);
 
         Intent vpn = VpnService.prepare(this);
         if (vpn != null) {
             startActivityForResult(vpn, 0);
         } else {
             startService(new Intent(this, MyVpnService.class));
+            startActivity(new Intent(this, PlayerActivity.class));
         }
+    }
+
+    @Override
+    protected void onActivityResult(int r, int c, Intent d) {
+        super.onActivityResult(r, c, d);
+        startService(new Intent(this, MyVpnService.class));
+        startActivity(new Intent(this, PlayerActivity.class));
     }
 }
